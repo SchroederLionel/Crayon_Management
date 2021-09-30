d630c323d15763fc330c60f44ba5e250117c319c
Lionel Schroeder <schroederlionel@gmail.com>
2021-09-30 15:47:34 +0200

    Setting up menu (Custom List Tile)

---
+++ lib/widgets/drawer_list_tile.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DrawerListTile extends StatelessWidget {
+  const DrawerListTile(
+      {Key? key,
+      required this.title,
+      required this.icon,
+      required this.pressed})
+      : super(key: key);
+  final String title;
+  final IconData icon;
+  final VoidCallback pressed;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: pressed,
+      horizontalTitleGap: 0.0,
+      leading: Icon(
+        icon,
+        color: Colors.white54,
+      ),
+      title: Text(
+        title,
+        style: const TextStyle(color: Colors.white54),
+      ),
+    );
+  }
+}
